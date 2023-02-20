9c7640eb6355d3f0cee7afb06defb9e4cf3083ef
Lei <774562910@qq.com>
2023-02-20 16:38:21 +0800

    first commit

---
+++ src/main/java/com/lei/GitTest.java
@@ -0,0 +1,11 @@
+package main.java.com.lei;
+
+/**
+ * @author:Lei
+ * @version:1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
